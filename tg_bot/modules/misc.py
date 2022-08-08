034700ebc1493c75e7b8c41a9e79dd1a8b4e14d8
Xelcius <mparashar210@gmail.com>
2022-08-08 06:30:12 -0700

    Update misc.py

--- tg_bot/modules/misc.py
+++ tg_bot/modules/misc.py
@@ -4,11 +4,31 @@ import time
 import git
 import requests
 from io import BytesIO
-from telegram import Chat, Update, MessageEntity, ParseMode, User
-from telegram.error import BadRequest
-from telegram.ext import Filters, CallbackContext
-from telegram.utils.helpers import mention_html, escape_markdown
+from telegram import Chat, MessageEntity, ParseMode, User
 from subprocess import Popen, PIPE
+import re
+import os
+import importlib
+from typing import List
+
+from psutil import cpu_percent, virtual_memory, disk_usage, boot_time
+from platform import python_version
+from telethon.tl.functions.channels import GetFullChannelRequest
+from telethon.tl.types import ChannelParticipantsAdmins
+from telethon.tl.types import InputMessagesFilterContacts, InputMessagesFilterDocument, InputMessagesFilterGeo, InputMessagesFilterGif, InputMessagesFilterMusic, InputMessagesFilterPhotos, InputMessagesFilterRoundVideo, InputMessagesFilterUrl, InputMessagesFilterVideo
+
+from telethon import events
+
+from pyrogram import filters
+from pyrogram.errors import PeerIdInvalid
+from pyrogram.types import Message, User
+
+from telegram import MAX_MESSAGE_LENGTH, Update, MessageEntity, __version__ as ptbver, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import (CallbackContext, CallbackQueryHandler, CommandHandler,
+                          Filters, MessageHandler)
+from telegram.ext.dispatcher import run_async
+from telegram.error import BadRequest
+from telegram.utils.helpers import escape_markdown, mention_html
 
 from tg_bot import (
     dispatcher,
@@ -20,8 +40,12 @@ from tg_bot import (
     WHITELIST_USERS,
     INFOPIC,
     sw,
-    StartTime
+    StartTime,
+    pgram
 )
+
+SUPPORT_CHAT = "AsukaSupport"
+
 from tg_bot.__main__ import STATS, USER_INFO, TOKEN
 from tg_bot.modules.sql import SESSION
 from tg_bot.modules.helper_funcs.chat_status import user_admin, sudo_plus
@@ -29,12 +53,16 @@ from tg_bot.modules.helper_funcs.extraction import extract_user
 import tg_bot.modules.sql.users_sql as sql
 from tg_bot.modules.users import __user_info__ as chat_count
 from tg_bot.modules.language import gs
-from telegram import __version__ as ptbver, InlineKeyboardMarkup, InlineKeyboardButton
-from psutil import cpu_percent, virtual_memory, disk_usage, boot_time
 import datetime
 import platform
 from platform import python_version
 from tg_bot.modules.helper_funcs.decorators import kigcmd, kigcallback
+from tg_bot.modules.disable import DisableAbleCommandHandler
+from tg_bot.modules.sql.antispam_sql import is_user_gbanned
+from tg_bot.modules.redis.afk_redis import is_user_afk, afk_reason
+from tg_bot.modules.sql.users_sql import get_user_num_chats
+from tg_bot import telethn
+
 
 MARKDOWN_HELP = f"""
 Markdown is a very powerful formatting tool supported by telegram. {dispatcher.bot.first_name} has some enhancements, to make sure that \
@@ -108,28 +136,102 @@ def gifid(update: Update, _):
     else:
         update.effective_message.reply_text("Please reply to a gif to get its ID.")
 
+def no_by_per(totalhp, percentage):
+    """
+    rtype: num of `percentage` from total
+    eg: 1000, 10 -> 10% of 1000 (100)
+    """
+    return totalhp * percentage / 100
+
+
+def get_percentage(totalhp, earnedhp):
+    """
+    rtype: percentage of `totalhp` num
+    eg: (1000, 100) will return 10%
+    """
+
+    matched_less = totalhp - earnedhp
+    per_of_totalhp = 100 - matched_less * 100.0 / totalhp
+    per_of_totalhp = str(int(per_of_totalhp))
+    return per_of_totalhp        
+
+def hpmanager(user):
+    total_hp = (get_user_num_chats(user.id) + 10) * 10
+
+    if not is_user_gbanned(user.id):
+
+        # Assign new var `new_hp` since we need `total_hp` in
+        # end to calculate percentage.
+        new_hp = total_hp
+
+        # if no username decrease 25% of hp.
+        if not user.username:
+            new_hp -= no_by_per(total_hp, 25)
+        try:
+            dispatcher.bot.get_user_profile_photos(user.id).photos[0][-1]
+        except IndexError:
+            # no profile photo ==> -25% of hp
+            new_hp -= no_by_per(total_hp, 25)
+        # if no /setme exist ==> -20% of hp
+        if not sql.get_user_me_info(user.id):
+            new_hp -= no_by_per(total_hp, 20)
+        # if no bio exsit ==> -10% of hp
+        if not sql.get_user_bio(user.id):
+            new_hp -= no_by_per(total_hp, 10)
+
+        if is_user_afk(user.id):
+            afkst = afk_reason(user.id)
+            # if user is afk and no reason then decrease 7%
+            # else if reason exist decrease 5%
+            new_hp -= no_by_per(total_hp, 7) if not afkst else no_by_per(total_hp, 5)
+            # fbanned users will have (2*number of fbans) less from max HP
+            # Example: if HP is 100 but user has 5 diff fbans
+            # Available HP is (2*5) = 10% less than Max HP
+            # So.. 10% of 100HP = 90HP
+
+    else:
+        new_hp = no_by_per(total_hp, 5)
+
+    return {
+        "earnedhp": int(new_hp),
+        "totalhp": int(total_hp),
+        "percentage": get_percentage(total_hp, new_hp),
+    }
+
+
+def make_bar(per):
+    done = min(round(per / 10), 10)
+    return "■" * done + "□" * (10 - done)
+                
 @kigcmd(command='info', pass_args=True)
-def info(update: Update, context: CallbackContext):  # sourcery no-metrics
-    bot = context.bot
-    args = context.args
+def info(update: Update, context: CallbackContext):
+    bot, args = context.bot, context.args
     message = update.effective_message
     chat = update.effective_chat
-    if user_id := extract_user(update.effective_message, args):
+    buttons = [
+    [
+                        InlineKeyboardButton(
+                             text="Health",
+                             url="https://t.me/AsukaUpdates/4"),
+                       InlineKeyboardButton(
+                             text="Disasters",
+                             url="https://t.me/AsukaUpdates/5"),
+                    ],
+    ]
+    user_id = extract_user(update.effective_message, args)
+
+    if user_id:
         user = bot.get_chat(user_id)
 
     elif not message.reply_to_message and not args:
-        user = (
-            message.sender_chat
-            if message.sender_chat is not None
-            else message.from_user
-        )
+        user = message.from_user
 
     elif not message.reply_to_message and (
         not args
         or (
             len(args) >= 1
             and not args[0].startswith("@")
-            and not args[0].lstrip("-").isdigit()
+            and not args[0].isdigit()
             and not message.parse_entities([MessageEntity.TEXT_MENTION])
         )
     ):
@@ -139,57 +241,121 @@ def info(update: Update, context: CallbackContext):  # sourcery no-metrics
     else:
         return
 
-    if hasattr(user, 'type') and user.type != "private":
-        text = get_chat_info(user)
-        is_chat = True
-    else:
-        text = get_user_info(chat, user)
-        is_chat = False
+    rep = message.reply_text("<code>Appraising...</code>", parse_mode=ParseMode.HTML)
+
+    text = (
+        f"╒═══「<b>• Appraisal Results •</b> 」\n\n"
+        f"• ID: <code>{user.id}</code>\n"
+        f"• First Name: {html.escape(user.first_name)}"
+    )
+
+    if user.last_name:
+        text += f"\n• Last Name: {html.escape(user.last_name)}"
+
+    if user.username:
+        text += f"\n• Username: @{html.escape(user.username)}"
+
+    text += f"\n• Userlink: {mention_html(user.id, 'link')}"
+
+    if chat.type != "private" and user_id != bot.id:
+        _stext = "\n• Presence: <code>{}</code>"
+
+        afk_st = is_user_afk(user.id)
+        if afk_st:
+            text += _stext.format("AFK")
+        else:
+            status = status = bot.get_chat_member(chat.id, user.id).status
+            if status:
+                if status in {"left", "kicked"}:
+                    text += _stext.format("Not here")
+                elif status == "member":
+                    text += _stext.format("Detected")
+                elif status in {"administrator", "creator"}:
+                    text += _stext.format("Admin")
+    if user_id not in [bot.id, 777000, 1087968824]:
+        userhp = hpmanager(user)
+        text += f"\n\n<b>• Health:</b> <code>{userhp['earnedhp']}/{userhp['totalhp']}</code>\n[<i>{make_bar(int(userhp['percentage']))} </i>{userhp['percentage']}%]"
+
+    try:
+        spamwtc = sw.get_ban(int(user.id))
+        if spamwtc:
+            text += "\n\n<b>This person is Spamwatched!</b>"
+            text += f"\nReason: <pre>{spamwtc.reason}</pre>"
+            text += "\nAppeal at @SpamWatchSupport"
+    except:
+        pass  # don't crash if api is down somehow...
+
+    disaster_level_present = False
+
+    if user.id == OWNER_ID:
+        text += "\n\n• Disaster Level: God"
+        disaster_level_present = True
+    elif user.id in DEV_USERS:
+        text += "\n\n• Disaster Level: Evangelion Master"
+        disaster_level_present = True
+    elif user.id in SUDO_USERS:
+        text += "\n\n• Disaster Level: Evangelion Pilot"
+        disaster_level_present = True
+    elif user.id in SUPPORT_USERS:
+        text += "\n\n• Disaster Level: Evangelion 3.0"
+        disaster_level_present = True
+    elif user.id in SERDEGNA_USERS:
+        text += "\n\n• Disaster Level: Evangelion 2.0"
+        disaster_level_present = True
+    elif user.id in WHITELIST_USERS:
+        text += "\n\n• Disaster Level: Evengalion 1.0"
+        disaster_level_present = True
+    try:
+        user_member = chat.get_member(user.id)
+        if user_member.status == "administrator":
+            result = requests.post(
+                f"https://api.telegram.org/bot{TOKEN}/getChatMember?chat_id={chat.id}&user_id={user.id}",
+            )
+            result = result.json()["result"]
+            if "custom_title" in result.keys():
+                custom_title = result["custom_title"]
+                text += f"\n\n• Title: <b>{custom_title}</b>"
+        
+    except BadRequest:
+        pass
+      
+    for mod in USER_INFO:
+        try:
+            mod_info = mod.__user_info__(user.id).strip()
+        except TypeError:
+            mod_info = mod.__user_info__(user.id, chat.id).strip()
+        if mod_info:
+            text += "\n\n" + mod_info
 
     if INFOPIC:
-        if is_chat:
-            try:
-                pic = user.photo.big_file_id
-                pfp = bot.get_file(pic).download(out=BytesIO())
-                pfp.seek(0)
-                message.reply_document(
-                        document=pfp,
-                        filename=f'{user.id}.jpg',
-                        caption=text,
-                        parse_mode=ParseMode.HTML,
-                )
-            except AttributeError:  # AttributeError means no chat pic so just send text
-                message.reply_text(
-                        text,
-                        parse_mode=ParseMode.HTML,
-                        disable_web_page_preview=True,
-                )
-        else:
-            try:
-                profile = bot.get_user_profile_photos(user.id).photos[0][-1]
-                _file = bot.get_file(profile["file_id"])
-
-                _file = _file.download(out=BytesIO())
-                _file.seek(0)
-
-                message.reply_document(
-                        document=_file,
-                        caption=(text),
-                        parse_mode=ParseMode.HTML,
-                )
-
-            # Incase user don't have profile pic, send normal text
-            except IndexError:
-                message.reply_text(
-                        text, parse_mode=ParseMode.HTML, disable_web_page_preview=True
-                )
+        try:
+            profile = context.bot.get_user_profile_photos(user.id).photos[0][-1]
+            context.bot.sendChatAction(chat.id, "upload_photo")
+            context.bot.send_photo(
+            chat.id,
+            photo=profile,
+            caption=(text),
+            reply_markup=InlineKeyboardMarkup(buttons),
+                parse_mode=ParseMode.HTML,            
+        )
+        # Incase user don't have profile pic, send normal text
+        except IndexError:
+            message.reply_text(
+            text,
+        reply_markup=InlineKeyboardMarkup(buttons),
+                parse_mode=ParseMode.HTML,            
+                   )
 
     else:
         message.reply_text(
-            text, parse_mode=ParseMode.HTML, disable_web_page_preview=True
-        )
+            text,
+        reply_markup=InlineKeyboardMarkup(buttons),
+                parse_mode=ParseMode.HTML,            
+                   )
 
+    rep.delete()
 
+"""
 def get_user_info(chat: Chat, user: User) -> str:
     bot = dispatcher.bot
     text = (
@@ -251,7 +417,7 @@ def get_user_info(chat: Chat, user: User) -> str:
         if mod_info:
             text += "\n" + mod_info
     return text
-
+"""
 
 def get_chat_info(user):
     text = (
